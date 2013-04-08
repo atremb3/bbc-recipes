144ae750d7599928979b27352b97f8d5e722dcce
annick <annick.tremblet@gmail.com>
2013-04-08 18:50:49 +0100

    search recipe functionality, client side

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -21,6 +21,45 @@ function applyFormStyling(element) {
     
 }
 
+function applyTableStyling() {
+	 $(".jtable th").each(function(){
+		 
+		  $(this).addClass("ui-state-default");
+		 
+		  });
+		 $(".jtable td").each(function(){
+		 
+		  $(this).addClass("ui-widget-content");
+		 
+		  });
+		 $(".jtable tr").hover(
+		     function()
+		     {
+		      $(this).children("td").addClass("ui-state-hover");
+		     },
+		     function()
+		     {
+		      $(this).children("td").removeClass("ui-state-hover");
+		     }
+		    );
+		// $(".jtable tr").click(function(){
+//		   
+//		   //$(this).children("td").toggleClass("ui-state-highlight");
+//		 // make line editable
+//		 
+//		 var idToUpdate = $(this).attr('id');
+//		 
+//		 myJsRoutes.controllers.Application.updateRecipeForm(idToUpdate).ajax({
+//			    success : function(data) { 
+//			    	//$(this).replaceWith(data);
+//			    	$('#1').replaceWith(data);
+//			    } 		
+//			});
+//		 
+//		  });
+		 
+}
+
 jQuery(function($){//on document ready
     //configureWidgets();
     //applyFormStyling();
@@ -28,4 +67,5 @@ jQuery(function($){//on document ready
     //enableColumnSorting();
     //enableShortcuts();
     //enableCheckboxSubmission();
+	applyTableStyling();
 });
